e57d4c485e230b50350e2bacdedc9e1c705b0367
Ilhammzz <ilham.abdulaziz20@gmail.com>
2023-08-17 12:44:36 +0700

    init

---
+++ resources/views/signin.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <title>Sign In</title>
+</head>
+<body>
+    <div class="container-signin">
+    <h1 style="padding-bottom:18.75px;">
+        Sign in
+    </h1>
+
+    <form action="">
+        <div class="" style="padding-bottom:24px;">
+            <input type="text" class="form-control" placeholder="NIK" style="border-style:solid; border-width:2px; border-color:#97CBCC;">  
+        </div>
+        <div class="" style="padding-bottom:33px;">
+            <input type="text" class="form-control" placeholder="Tanggal Lahir" style="border-style:solid; border-width:2px; border-color:#97CBCC;">
+        </div>
+        <button class="btn-daftar rounded-3" type="submit"> Daftar </button>
+    </form>
+    </div>
+</body>
+
+<style>
+    .container-signin {
+        padding-right : 25.1px;
+        padding-left : 25.1px;
+        padding-top : 21.12px;
+    }
+
+    .h1-signin {
+        font-size : 30px;
+        font-family : "Roboto";
+        font-style : bold;
+    }
+
+    .btn-daftar {
+        width: 100%;
+        height: 40px;
+        background-color: #01B7BB;
+        color : #ffffff;
+        border-style: none;
+        font-family : "Roboto";
+        font-style : bold;
+    }
+
+</style>
+</html>
